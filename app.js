05d84be6ea3a5c9ef169e5fe7a2ab1999d4dd294
cocojayoo <tlsdl119@naver.com>
2022-11-02 16:46:46 +0900

    221102

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
+const modeBtn = document.getElementById("mode-btn");
+const destroyBtn = document.getElementById("destroy-btn");
+const eraserBtn = document.getElementById("eraser-btn");
+const colorOption = Array.from(document.getElementsByClassName("color-option"));
 const canvas = document.querySelector("canvas");
-
+const lineWidth = document.querySelector("#line-width");
+const color = document.querySelector("#color");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 800;
-canvas.height = 800;
+const CANVAS_WIDTH = 800;
+const CANVAS_HEIGHT = 800;
 
-ctx.lineWidth = 2;
+canvas.width = CANVAS_WIDTH;
+canvas.height = CANVAS_HEIGHT;
+ctx.lineWidth = 5;
 let isPainting = false;
+let isFilling = false;
+
 function onMove(event){
     if (isPainting) {
         ctx.lineTo(event.offsetX, event.offsetY);
@@ -20,11 +29,63 @@ function startPainting(){
 }
 function cancelPainting(){
     isPainting = false;
+    ctx.beginPath();
+}
+function onLineWidthChange(event){
+    ctx.lineWidth = event.target.value;
+}
+function onColorChange(event){
+    ctx.strokeStyle = event.target.value;
+    ctx.fillStyle = event.target.value;    
+}
+
+function onColorClick(event){
+    const colorValue = event.target.dataset.color;
+    ctx.strokeStyle = colorValue;
+    ctx.fillStyle = colorValue;
+    color.value = colorValue;
+    
+}
+
+function onModeClick(){
+if(isFilling){
+    isFilling = false
+    modeBtn.innerText ="Fill";
+}else{
+    isFilling = true
+    modeBtn.innerText ="Draw";
+}
+}
+function onCanvasClick() {
+    if(isFilling){
+        ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    }
+}
+function onDestroyClick() {
+    ctx.fillStyle = "white"
+    ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+}
+
+function onEraserClick(){
+    ctx.strokeStyle = "white";
+    isFilling = false;
+    modeBtn.innerText ="Fill";
 }
 canvas.addEventListener("mousemove", onMove);
 canvas.addEventListener("mousedown", startPainting);
 canvas.addEventListener("mouseup", cancelPainting);
 canvas.addEventListener("mouseleave", cancelPainting);
+canvas.addEventListener("click", onCanvasClick);
+color.addEventListener("change", onColorChange);
+lineWidth.addEventListener("change", onLineWidthChange);
+
+colorOption.forEach((color) => color.addEventListener("click", onColorClick));
+
+modeBtn.addEventListener("click", onModeClick);
+destroyBtn.addEventListener("click", onDestroyClick);
+eraserBtn.addEventListener("click", onEraserClick);
+
 // const colors = [
 //     "#ff3838",
 //     "#ffb8b8",
